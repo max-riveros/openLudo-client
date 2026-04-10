c6d118d722062999dafd2d35d0805f6d993c731d
Johannes Gehrig Riveros <uoolv@student.kit.edu>
2026-04-10 06:22:20 +0200

    Make App.tsx for testing

--- App.tsx
+++ App.tsx
@@ -1,44 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import GameModule from './specs/NativeGameModule';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+function App(): React.JSX.Element {
+  console.log(Platform.OS);
+  const [reversedValue, setReversedValue] = React.useState('');
+
+  // ----- Events -----
+
+  GameModule.registerCallback((event) => {
+    switch (event.type) {
+      case "gameStart":
+        setReversedValue(reversedValue + event.type);
+        break;
+    
+      case "playerTurn":
+        setReversedValue(reversedValue + "\n" + event.type + ": " + event.playerId);
+        break;
+    
+      case "diceRolled":
+        setReversedValue(reversedValue + "\n" + event.type + ": " + event.value);
+        break;
+    
+      case "gameOver":
+        setReversedValue(reversedValue + "\n" + event.type);
+        break;
+    
+      case "pawnKilled":
+        setReversedValue(reversedValue + "\n" + event.type + ": killer " + event.killerId);
+        break;
+    
+      case "pawnMoved":
+        setReversedValue(reversedValue + "\n" + event.type + ": from " + event.fromPosition + " to " + event.toPosition);
+        break;
+    
+      case "pawnMovedToGoalArea":
+        setReversedValue(reversedValue + "\n" + event.type + ": to " + event.position);
+        break;
+    
+      case "pawnRevived":
+        setReversedValue(reversedValue + "\n" + event.type + ": pawn is at " + event.position);
+        break;
+    
+      case "pawnSaved":
+        setReversedValue(reversedValue + "\n" + event.type);
+        break;
+    
+      case "playerSkipped":
+        setReversedValue(reversedValue + "\n" + event.type);
+        break;
+    
+      case "selected":
+        setReversedValue(reversedValue + "\n" + event.type + ": " + event.pawnId);
+        break;
+    
+      case "waitingForSelect":
+        setReversedValue(reversedValue + "\n" + event.type);
+        break;
+    
+      case "waitingForDice":
+        setReversedValue(reversedValue + "\n" + event.type);
+        break;
+    
+      default:
+        break;
+    }
+  });
+
+  // ------------------
+
+  const onConnect = () => {
+    GameModule.connect();
+  };
+  const onDice = () => {
+    GameModule.rollDice();
+  };
+  const onSelect = () => {
+    GameModule.selectPawn(1);
+  };
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+      <View style={{backgroundColor: 'blue'}}>
+        <Text style={styles.title}>
+          Welcome to C++ Turbo Native Module Example
+        </Text>
+        <Button title="Connect" onPress={onConnect} />
+        <Button title="Dice" onPress={onDice} />
+        <Button title="Select 1" onPress={onSelect} />
+        <Text>text: {reversedValue}</Text>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 10,
   },
 });
 
